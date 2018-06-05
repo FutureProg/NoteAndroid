3dae6314e5ceb63d671448eb15f9de2af8d611d5
Nickolas Morrison <nmorrison@konradgroup.com>
2018-06-05 13:27:34 -0400

    Giving ability to add notes

--- app/src/main/java/com/example/nickolasmorrison/mynote/MainActivity.java
+++ app/src/main/java/com/example/nickolasmorrison/mynote/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.nickolasmorrison.mynote;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.nickolasmorrison.mynote.data.NoteViewModel;
+import com.example.nickolasmorrison.mynote.views.fragments.MainFragment;
+
+public class MainActivity extends AppCompatActivity implements MainFragment.InteractionListener {
+
+    NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = this.getSupportFragmentManager();
+        MainFragment mainFragment = MainFragment.newInstance();
+        manager.beginTransaction().replace(R.id.fragment_container,mainFragment).commit();
     }
 
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
--- app/src/main/java/com/example/nickolasmorrison/mynote/views/fragments/MainFragment.java
+++ app/src/main/java/com/example/nickolasmorrison/mynote/views/fragments/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.nickolasmorrison.mynote.views.fragments;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.nickolasmorrison.mynote.R;
+import com.example.nickolasmorrison.mynote.data.NoteViewModel;
+import com.example.nickolasmorrison.mynote.storage.Note;
+import com.example.nickolasmorrison.mynote.storage.NoteRepository;
+import com.example.nickolasmorrison.mynote.views.NoteListAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,18 +26,14 @@ import com.example.nickolasmorrison.mynote.R;
  */
 public class MainFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private InteractionListener mListener;
+    private NoteListAdapter listAdapter;
+    private NoteViewModel noteVM;
 
     public MainFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainFragment newInstance() {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
@@ -45,27 +46,32 @@ public class MainFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
         }
+        noteVM = ViewModelProviders.of(this).get(NoteViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        listAdapter = new NoteListAdapter(this.getContext());
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(listAdapter);
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public void addNote(View view) {
+        Note note = new Note();
+        note.text = "Hello";
+        note.title = "Hello World";
+        noteVM.insert(note);
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof InteractionListener) {
+            mListener = (InteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -88,7 +94,7 @@ public class MainFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface InteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
